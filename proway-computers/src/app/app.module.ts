5202927be2b42ae345f0aab95359c0e3e3ff32a7
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2022-12-04 11:31:23 -0300

    :sparkles: feat Page Not Found

--- proway-computers/src/app/app.module.ts
+++ proway-computers/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
