d4abc096777b6106fbce53c3ecab6f167d983620
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2022-12-04 14:39:06 -0300

    :sparkles: feat Cart Page

--- proway-computers/src/app/app-routing.module.ts
+++ proway-computers/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
   { path: "", redirectTo: "products", pathMatch: "full" },
+  { path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule) },
   { path: "**", component: PageNotFoundComponent}
 ];
 
