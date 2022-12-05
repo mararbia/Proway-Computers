9dc868231661577f1b7dde8c9696f4ad3aa74f09
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2022-12-04 22:41:45 -0300

    :sparkles: feat Contact Page - Field validation

--- proway-computers/src/app/contact/contact.component.ts
+++ proway-computers/src/app/contact/contact.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  contactForm = this.fb.group({
+    name: ['', [Validators.minLength(4), Validators.required]],
+    topic: ['', [Validators.minLength(10), Validators.required]],
+    phone: ['', [Validators.minLength(11), Validators.required]],
+    email: ['', [Validators.email, Validators.required]],
+    message: ['', [Validators.minLength(20), Validators.required]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  sendForm(){
+    alert("A mensagem foi enviada!");
+    this.contactForm.reset();
+  }
 }
